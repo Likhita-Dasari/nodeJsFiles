062b8c3ab3321366fb18e87084ab447c8c9f50eb
Likhita-Dasari <159152790+Likhita-Dasari@users.noreply.github.com>
2024-08-20 23:33:33 +0530

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const path = require('path')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const dbPath = path.join(__dirname, 'moviesData.db')
+let db = null
+const initializeTheServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3001, () => {
+      console.log('Server is running Successfully at https://localhost:3001/')
+    })
+  } catch (e) {
+    console.log(`Caught an error ${e.Message}`)
+  }
+}
+initializeTheServer()
+
+const convertToCamelCase = movie_id => {
+  return {
+    movieName: movie_id,
+  }
+}
+//API GET METHOD
+app.get('/movies/', async (request, response) => {
+  let databaseCommand = `SELECT movie_name FROM 
+        movie`
+  const movieNameArray = await db.all(databaseCommand)
+  response.send(
+    movieNameArray.map(eachName => {
+      convertToCamelCase(eachName.movie_id)
+    }),
+  )
+})
+module.exports = app
